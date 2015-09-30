66027ab98d730da0cbd3d39fe1b1916c74172242
Regnator <legendwyvernkiller@googlemail.com>
2015-09-30 15:49:36 +0200

    First commit

---
+++ app/src/main/java/de/united_gaming/regnator/win1splan/Win1Splan.java
@@ -0,0 +1,62 @@
+package de.united_gaming.regnator.win1splan;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+
+public class Win1Splan extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webview = new WebView(this);
+        webview.getSettings().setBuiltInZoomControls(true);
+        webview.getSettings().setJavaScriptEnabled(false);
+
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webview.loadUrl("https://splan.hs-heilbronn.de/splan/std?act=tt&lan=de&pu=14&sel=pg&og=424&pg=WIN+1&sd=true&sd2=Aktuelle+Woche");
+        setContentView(webview);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_win1_splan, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
